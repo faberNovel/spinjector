8b3addddbab5a6b8f913fd767776c6544e39d88b
Pierre Felgines <pierre.felgines@fabernovel.com>
2022-02-07 13:21:58 +0100

    Add empty iOS project

---
+++ test/fixtures/EmptyProject/EmptyProject/EmptyProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmptyProjectApp.swift
+//  EmptyProject
+//
+//  Created by Pierre Felgines on 07/02/2022.
+//
+
+import SwiftUI
+
+@main
+struct EmptyProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
